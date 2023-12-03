7ed2978e335d9fe9101b62917968a4b08a395320
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2023-12-03 18:50:51 +0330

    add index to object_uuid

--- Modules/DataProcessor/database/migrations/2023_12_01_140255_create_process_histories_table.php
+++ Modules/DataProcessor/database/migrations/2023_12_01_140255_create_process_histories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('process_histories', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-            $table->uuid('object_uuid');
+            $table->uuid('object_uuid')->index();
             $table->unsignedBigInteger('object_volume')->comment('byte volume size in a month');
             $table->timestamps();
         });

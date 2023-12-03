72bd729d01b48b9e69b8b473f9319fb08924fb50
Mehdi Jalali <mehdi.jalalii03@gmail.com>
2023-12-03 23:29:48 +0330

    add monthly scope

--- Modules/DataProcessor/app/Models/ProcessHistory.php
+++ Modules/DataProcessor/app/Models/ProcessHistory.php
@@ -3,6 +3,7 @@
 namespace Modules\DataProcessor\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\DataProcessor\Database\factories\ProcessHistoryFactory;
 
@@ -15,9 +16,13 @@ class ProcessHistory extends Model
      */
     protected $fillable = ['user_id', 'object_uuid', 'object_volume'];
 
-
     protected static function newFactory(): ProcessHistoryFactory
     {
         return ProcessHistoryFactory::new();
+    }
+
+    public function scopeMonthly(Builder $query): void
+    {
+        $query->whereDate('created_at', '>' , now()->startOfMonth());
     }
 }
